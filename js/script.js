5f494a569af3b29eb0f69ac8d3743cb3dd64c56c
sajiwaniDewi <dewisanjiw@gmail.com>
2024-01-27 10:50:25 +0900

    unused code

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
-const branches = [
-    {
-        area: "Jakarta",
-        address: [
-            "Jl Pegangsaan timur No 48, Jakarta Selatan",
-            "Jl Melati indah mekar No 56, Tanjung Duren",
-            "Jl Budi pekerti luhur No 82,  Jakarta Utara",
-            "Jl Akar gandaria No 45, Jakarta Barat",
-        ],
-    },
-    {
-        area: "Bandung",
-        address: [
-            "Jl Dago pakar No 05, Dago",
-            "Jl A. Yani No 45, Asia Afrika",
-            "Jl Pelajar Pejuang No 54, Buah Batu",
-        ],
-    },
-    {
-        area: "Bogor",
-        address: [
-            "Jl Dayang Sumbi No 45, Cisarua",
-            "Jl Sultan Hasan No 54, Bogor",
-        ],
-    },
-    {
-        area: "Tangerang",
-        address: [
-            "Jl Hos Cokrominoto No 109",
-            "Jl BSD grand Boulevard no 45, BSD",
-            "Jl Letnan Sutopo 08, Tangerang Selatan",
-        ],
-    },
-];
+// const branches = [
+//     {
+//         area: "Jakarta",
+//         address: [
+//             "Jl Pegangsaan timur No 48, Jakarta Selatan",
+//             "Jl Melati indah mekar No 56, Tanjung Duren",
+//             "Jl Budi pekerti luhur No 82,  Jakarta Utara",
+//             "Jl Akar gandaria No 45, Jakarta Barat",
+//         ],
+//     },
+//     {
+//         area: "Bandung",
+//         address: [
+//             "Jl Dago pakar No 05, Dago",
+//             "Jl A. Yani No 45, Asia Afrika",
+//             "Jl Pelajar Pejuang No 54, Buah Batu",
+//         ],
+//     },
+//     {
+//         area: "Bogor",
+//         address: [
+//             "Jl Dayang Sumbi No 45, Cisarua",
+//             "Jl Sultan Hasan No 54, Bogor",
+//         ],
+//     },
+//     {
+//         area: "Tangerang",
+//         address: [
+//             "Jl Hos Cokrominoto No 109",
+//             "Jl BSD grand Boulevard no 45, BSD",
+//             "Jl Letnan Sutopo 08, Tangerang Selatan",
+//         ],
+//     },
+// ];
 // const showBranches = document.getElementById("branch");
 
 // for (let i = 1; i <= branches.length; i++) {
